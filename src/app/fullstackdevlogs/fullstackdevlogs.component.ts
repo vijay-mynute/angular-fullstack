33223c4950e20cde683b88f0603cf1af6a6319e9
vijay-mynute <vijay.mynute@gmail.com>
2022-07-23 22:53:41 +0530

    source modification

---
+++ src/app/fullstackdevlogs/fullstackdevlogs.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-fullstackdevlogs',
+  templateUrl: './fullstackdevlogs.component.html',
+  styleUrls: ['./fullstackdevlogs.component.css']
+})
+export class FullstackdevlogsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
