a4ea719dd88543786df5c7af2b399654a10feea8
vijay-mynute <vijay.mynute@gmail.com>
2022-07-23 23:54:09 +0530

    1 file modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,11 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
-//import { FullStackDevLogs } from './fullstackdevlogs.component';
+import { FullStackDevLogs } from './fullstackdevlogs/fullstackdevlogs.component';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
+  declarations: [ AppComponent, HelloComponent, FullStackDevLogs ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
